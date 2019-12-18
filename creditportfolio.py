816dc224097b0f20de6ce296dc57a463d7ed9b68
Philippe LÊ <philippele@iMac-de-Philippe.local>
2019-12-18 22:43:50 +0100

    Committing before creating a new branch.

--- creditportfolio.py
+++ creditportfolio.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import matlib
 from numpy import random
 from typing import List
+import pandas as pd
 
 class CreditContract :
     def __init__(self, id, exposure, pd) :
@@ -9,14 +10,39 @@ class CreditContract :
         self.exposure = exposure
         self.pd = pd
         
+    def __repr__(self):
+        return(str((self.id, self.exposure, self.pd)))
+
+class RandomCreditContract(CreditContract) : 
+    # def __init__(self):
+    #     self.id = 
+    pass
+        
+class CreditPortfolioBuilder :
+    def __init__(self):
+        self.portfolio = None
+    
+    def portfolio_builder(self):
+        portfolio = CreditPortfolio()
+    
+    def portfolio_item(self) :
+        return(CreditPortfolioBuilder())
+    
+    pass
+        
 class CreditPortfolio : 
     def __init__(self) :
-        self.portfolio = [] # list of CreditContract
+        self.portfolio = [] # pd.DataFrame
     
+    def __repr__(self):
+        return(str(list(map(str, self.portfolio))))
+        
     def add_contract_list(self, contract : List[CreditContract]):
         self.portfolio.extend(contract)
-        
-        
+    
+    
+     
+    
 class ExposuresSimulation :
     def __init__(self, size, alpha) :
         self.Exposures = np.random.pareto(alpha, size)
--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 import creditportfolio as cp
 import gaussian_copula as gc
 import timing as mytime
-
+import pandas as pd
+import collections
 if __name__ == "__main__":
     rho = 0.2
     dim = 1
     Sig = gc.UniformCorrMatrix(rho, dim)
     X = gc.Gaussian_Vector_Simulation(dim, Sig)
     X.Simulate()
-    mytime.timing_it(X.Simulate, 10000)
+    mytime.timing_it(X.Simulate, 100)
     
-    ids = [104190 ,101044, 183410]
-    exposures = [1000, 2000, 500]
-    pds = [0.02, 0.03, 0.01]
+    ids = [104190 ,101044, 183410, 104190]
+    exposures = [1000, 2000, 500, 444]
+    pds = [0.02, 0.03, 0.01, 0.2]
+    
+    CreditContract = collections.namedtuple('CreditContract', 'id exposure pd')
+    cc1 = CreditContract(104190, 1000, 0.02)
+    cc2 = CreditContract(101044, 2000, 0.03)
+    print(cc1, cc2)
+
+    param_dict = {'id' : ids, 
+                  'exposure' : exposures, 
+                  'pd' : pds,
+                }
+    
+    my_pf = pd.DataFrame(param_dict)
+    
+    aggregate_by =  my_pf.groupby('id').agg({'exposure' : ['sum']})
+    print(aggregate_by)
+
+
     parameters = zip(ids, exposures, pds)
     fmap_contracts = lambda i_e_p : cp.CreditContract(i_e_p[0], i_e_p[1], i_e_p[2])
     List_of_contracts = map(fmap_contracts, parameters)
     
-    
+   
 
-    # print(List_of_contracts)
+
+    Portfolio = cp.CreditPortfolio()
+    Portfolio.add_contract_list(List_of_contracts)
+    print(Portfolio)
+        
     
-    # Contract1 = mccr.CreditContract(104190, 1000, 0.02)
-    # Contract2 = mccr.CreditContract(101044, 2000, 0.03)
-    # Contract3 = mccr.CreditContract(183410, 500, 0.01)
-    # Portfolio = mccr.CreditPortfolio()
-    # List_of_contracts = [Contract1 , Contract2, Contract3]
-    # Portfolio.add_contract_list(List_of_contracts)
