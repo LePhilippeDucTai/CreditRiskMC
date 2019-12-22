037afeded2194656efa09f19a50d885ba9e92883
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2019-12-22 09:05:15 +0100

    CreditPortfolio as a pandas dataframe. Methd for randomly generate a portfolio.

--- main.py
+++ main.py
@@ -3,43 +3,19 @@ import gaussian_copula as gc
 import timing as mytime
 import pandas as pd
 import collections
+import numpy as np
+
+from pandasgui import show
+
 if __name__ == "__main__":
     rho = 0.2
-    dim = 1
+    dim = 10
     Sig = gc.UniformCorrMatrix(rho, dim)
     X = gc.Gaussian_Vector_Simulation(dim, Sig)
     X.Simulate()
-    mytime.timing_it(X.Simulate, 100)
-    
-    ids = [104190 ,101044, 183410, 104190]
-    exposures = [1000, 2000, 500, 444]
-    pds = [0.02, 0.03, 0.01, 0.2]
-    
-    CreditContract = collections.namedtuple('CreditContract', 'id exposure pd')
-    cc1 = CreditContract(104190, 1000, 0.02)
-    cc2 = CreditContract(101044, 2000, 0.03)
-    print(cc1, cc2)
 
-    param_dict = {'id' : ids, 
-                  'exposure' : exposures, 
-                  'pd' : pds,
-                }
-    
-    my_pf = pd.DataFrame(param_dict)
-    
-    aggregate_by =  my_pf.groupby('id').agg({'exposure' : ['sum']})
-    print(aggregate_by)
-
-
-    parameters = zip(ids, exposures, pds)
-    fmap_contracts = lambda i_e_p : cp.CreditContract(i_e_p[0], i_e_p[1], i_e_p[2])
-    List_of_contracts = map(fmap_contracts, parameters)
-    
-   
-
-
-    Portfolio = cp.CreditPortfolio()
-    Portfolio.add_contract_list(List_of_contracts)
-    print(Portfolio)
-        
-    
+    n = 15
+    seed = 10293
+    CreditPort = cp.CreditPortfolioGen(seed = seed, size = n)
+    CreditPort.print()
+    # show(df)
---
+++ monte_carlo_credit.py
@@ -0,0 +1,29 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+# Model : abstract class
+# Subclasses : - Vasicek Model ? (Copula ?)
+#              - Student
+#              - Clayton
+
+
+class RiskFactor(ABC):
+    def __init__(self, seed):
+        self.seed = seed
+        super().__init__()
+
+    @abstractmethod
+    def simulate(self):
+        pass
+
+class Model(ABC):
+    def __init__(self, *args, **kwargs):
+        pass
+
+class SystemicRiskFactor(RiskFactor):
+    def simulate(self):
+        pass
+        
+class IdiosyncraticRiskFactor(RiskFactor):
+    pass
+    
