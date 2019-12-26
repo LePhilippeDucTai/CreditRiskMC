67e8252a1a42aa212d80296f4372dcf2f43b54a0
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2019-12-26 18:50:58 +0100

    Modify to integrate arrays of exposures and pds.

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ if __name__ == "__main__":
     n = 10000
     n_scenarios = 10000
 
+    RngContract = cp.RandomCreditContractGen(19481)
+    first = RngContract.random_contract_ts()
+    second = RngContract.random_contract_ts()
+    data = [first, second]
+    df = pd.DataFrame(data)
+
     CreditPort = cp.CreditPortfolioGen(seed = 10293, size = n)
+    
+    
+    '''
     Vasicek = mccr.VasicekModel(seed = 121414, data = CreditPort.portfolio, rho = 0.5)
     MC = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
     
     x = MC.simulate()
     xx = MC.simulate_parallel()
-
+    '''
     # print(x)
     # print(xx)
