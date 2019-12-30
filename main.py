28f5774cdd6961d7b4592dca6e80de6be979a158
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2019-12-30 22:05:42 +0100

    Removed multiprocessing for generating a credit portfolio.
    - Corrected the simulation of the systemic variable
    - Replaced np.sum of a product by np.dot
    - seeds for the monte carlo simulation are generated inside the simulate function.

--- main.py
+++ main.py
@@ -8,23 +8,18 @@ from pandasgui import show
 
 if __name__ == "__main__":
     n = 10000
-    n_scenarios = 10000
-
-    RngContract = cp.RandomCreditContractGen(19481)
-    first = RngContract.random_contract_ts()
-    second = RngContract.random_contract_ts()
-    data = [first, second]
-    df = pd.DataFrame(data)
+    n_scenarios = 1000
 
     CreditPort = cp.CreditPortfolioGen(seed = 10293, size = n)
+    # print(CreditPort)
     
-    
-    '''
+
     Vasicek = mccr.VasicekModel(seed = 121414, data = CreditPort.portfolio, rho = 0.5)
     MC = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
-    
+    MCparallel = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
+
     x = MC.simulate()
-    xx = MC.simulate_parallel()
-    '''
-    # print(x)
-    # print(xx)
+    xx = MCparallel.simulate_parallel()
+    
+    print(x == xx)
+    
