28da501b688217b08c84d6360cdc25a37e37d2f1
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2019-12-31 09:30:10 +0100

    Modified the way the multiprocessing works.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import gaussian_copula as gc
 import monte_carlo_credit as mccr
 import pandas as pd
 import numpy as np
-
+import functools
 from pandasgui import show
 
 if __name__ == "__main__":
-    n = 10000
+    n = 1000
     n_scenarios = 1000
 
     CreditPort = cp.CreditPortfolioGen(seed = 10293, size = n)
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     MC = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
     MCparallel = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
 
-    x = MC.simulate()
+    x = MC.simulate(n_scenarios, 19841)
     xx = MCparallel.simulate_parallel()
-    
-    print(x == xx)
+
+    print(len(x), len(xx))
     
