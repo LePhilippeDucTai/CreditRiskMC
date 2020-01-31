d79f9c406fb9f6535ae4b6e2e0e55ad8e8cf4eb9
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2020-01-31 04:32:12 +0100

    Added some formatting in profiling performances.

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import monte_carlo_credit as mccr
 import pandas as pd
 import numpy as np
 import functools
-from pandasgui import show
+# from pandasgui import show
+import os
 
 if __name__ == "__main__":
-    n = 1000
-    n_scenarios = 20000
+    n = 10
+    n_scenarios = 2000
 
     CreditPort = cp.CreditPortfolioGen(seed = 10293, size = n)
-    # print(CreditPort)
+    print(CreditPort)
     
-
     Vasicek = mccr.VasicekModel(seed = 121414, data = CreditPort.portfolio, rho = 0.5)
     MC = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
     MCparallel = mccr.MonteCarloEngine(model = Vasicek, n_scenarios = n_scenarios)
 
     x = MC.simulate(seed = 19841)
     xx = MCparallel.simulate_parallel()
+    # print(len(x), len(xx))
 
-    print(len(x), len(xx))
-    
+
--- timing.py
+++ timing.py
@@ -1,14 +1,52 @@
 import time
+import os
+import psutil
+import inspect
+
+def elapsed_since(start):
+    #return time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
+    elapsed = time.perf_counter() - start
+    if elapsed < 1:
+        return str(round(elapsed*1000,2)) + "ms"
+    if elapsed < 60:
+        return str(round(elapsed, 2)) + "s"
+    if elapsed < 3600:
+        return str(round(elapsed/60, 2)) + "min"
+    else:
+        return str(round(elapsed / 3600, 2)) + "hrs"
+
+
+def format_bytes(bytes):
+    if abs(bytes) < 1000:
+        return str(bytes)+"B"
+    elif abs(bytes) < 1e6:
+        return str(round(bytes/1e3,2)) + "kB"
+    elif abs(bytes) < 1e9:
+        return str(round(bytes / 1e6, 2)) + "MB"
+    else:
+        return str(round(bytes / 1e9, 2)) + "GB"
+
+
+
+def get_process_memory():
+    process = psutil.Process(os.getpid())
+    mi = process.memory_info()
+    return mi.rss
+
 
 def time_it(func) :
     def wrapper(*args, **kwargs):
         print(f'\n### Entering in {func.__name__}. ###')
         t1 = time.perf_counter()
+        rss_before = get_process_memory()
         s = func(*args, **kwargs)
-        t2 = time.perf_counter()
-        elapsed = t2 - t1
-        print(f'### Quitting function {func.__name__}. ###')
-        print(f'### Elapsed time : {elapsed : .10f} seconds. ### \n')
+        elapsed_time = elapsed_since(t1)
+        rss_after = get_process_memory()
+        print(f'### Quitting function {func.__name__}. ###\n')
+        print("Profiling: {:>8}  RSS: {:>8} | time: {:>8}".format("<" + func.__name__ + ">",
+            format_bytes(rss_after - rss_before),
+            elapsed_time))
+        print('\n')
         return(s)
     return(wrapper)
 
