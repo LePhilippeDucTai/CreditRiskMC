323fb4d21b11dd1c24fb43a16dfd1f2323816ee5
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2020-06-02 17:04:02 +0200

    Added a print inside the MC Engine.

--- monte_carlo_engine.py
+++ monte_carlo_engine.py
@@ -4,10 +4,12 @@ import timing
 class MonteCarloEngine:
     def __init__(self, n_simulations, model):
         self.n_simulations = n_simulations
-        self.pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        self.npools = multiprocessing.cpu_count()
+        self.pool = multiprocessing.Pool(self.npools)
         self.model = model
 
     @timing.time_it
     def compute(self):
+        print(f"Monte-Carlo computing with {self.npools} processors.")
         result = self.pool.map(self.model.simulate, range(self.n_simulations))
         return result
